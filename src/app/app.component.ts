0f9958eb444149609ca5d7f26b33999f9c750822
Sergio <sduarte2612@gmail.com>
2022-05-30 20:37:46 +0300

    fix files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loginapidoc';
+  
+  constructor(private service: AppServiceService) {}
+  ngOnInit(): void {
+    this.getDataFromAPI();
+  }
+
+  getDataFromAPI() {
+    this.service.getData().subscribe((response) => {
+      console.log('Response from Api', response);
+    }, (error) => {
+      console.log('Error is ', error)
+    })
+  }
 }
